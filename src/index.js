09a9fccffc3df4241ff020eab7e3ce0065f42e45
Taturick <noturno2drick@gmail.com>
2023-10-04 09:00:02 -0300

    Cronometro Online

--- src/index.js
+++ src/index.js
@@ -3,15 +3,11 @@ const path = require('path');
 
 const app = express();
 
-// Configurar o middleware para servir arquivos estáticos na pasta "public"
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Definir a rota principal
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'paginainicial.html'));
 });
-
-// Resto do seu código
 
 app.listen(3000, () => {
     console.log('Servidor rodando em http://localhost:3000');
